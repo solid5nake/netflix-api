c1bee6837aa57554c491a75934e78b416f747d92
Abraham Engel <engelenburg1980@gmail.com>
2021-01-11 23:25:04 +0100

    Initial commit

---
+++ src/test/java/com/netflix/api/NetflixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
