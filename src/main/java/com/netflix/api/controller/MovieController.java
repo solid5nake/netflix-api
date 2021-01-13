5e817db29aab0541da77da5255ea68b63e28005f
Abraham Engel <engelenburg1980@gmail.com>
2021-01-13 02:48:24 +0100

    Add Movie service

--- src/main/java/com/netflix/api/controller/MovieController.java
+++ src/main/java/com/netflix/api/controller/MovieController.java
@@ -1,8 +1,21 @@
 package com.netflix.api.controller;
 
+import com.netflix.api.dto.MovieDetailsDto;
+import com.netflix.api.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class MovieController {
+
+  @Autowired
+  private MovieService service;
+
+  @GetMapping("/movie/{movieId}")
+  public MovieDetailsDto getMovie(@PathVariable String movieId) {
+    return service.getMovie(movieId);
+  }
 }
