aee3fe1f40cbb099db07cb2adfe276c163b4dcdf
Abraham Engel <engelenburg1980@gmail.com>
2021-01-13 02:46:22 +0100

    Add movie service for the movie details

--- src/main/java/com/netflix/api/service/MovieService.java
+++ src/main/java/com/netflix/api/service/MovieService.java
@@ -1,7 +1,18 @@
 package com.netflix.api.service;
 
-import lombok.Value;
+import com.netflix.api.client.MovieClient;
+import com.netflix.api.dto.MovieDetailsDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MovieService {
 
+  @Autowired
+  private MovieClient client;
+
+  public MovieDetailsDto getMovie(String movieId) {
+    MovieDetailsDto movie = client.getMovieDetails(movieId,"f0762b04aa2c57fcee36e57e453488ed");
+    return movie;
+  }
 }
