605700eb4126660e5d1cb8a75799eba2c1b6f991
Abraham Engel <engelenburg1980@gmail.com>
2021-01-19 11:48:38 +0100

    Add MovieThumb model object/class

---
+++ src/main/java/com/netflix/api/dto/Moviethumb.java
@@ -0,0 +1,56 @@
+
+package com.netflix.api.dto;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Moviethumb {
+
+    private String id;
+    private String url;
+    private String lang;
+    private String likes;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+    public void setLikes(String likes) {
+        this.likes = likes;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
