76234527898e88f20d537c8502c523e8f21cda6c
Abraham Engel <engelenburg1980@gmail.com>
2021-01-19 11:50:03 +0100

    Create HDmovielogo model object/class

---
+++ src/main/java/com/netflix/api/dto/Hdmovielogo.java
@@ -0,0 +1,79 @@
+
+package com.netflix.api.dto;
+
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id",
+    "url",
+    "lang",
+    "likes"
+})
+public class Hdmovielogo {
+
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("url")
+    private String url;
+    @JsonProperty("lang")
+    private String lang;
+    @JsonProperty("likes")
+    private String likes;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("url")
+    public String getUrl() {
+        return url;
+    }
+
+    @JsonProperty("url")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @JsonProperty("lang")
+    public String getLang() {
+        return lang;
+    }
+
+    @JsonProperty("lang")
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    @JsonProperty("likes")
+    public String getLikes() {
+        return likes;
+    }
+
+    @JsonProperty("likes")
+    public void setLikes(String likes) {
+        this.likes = likes;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
