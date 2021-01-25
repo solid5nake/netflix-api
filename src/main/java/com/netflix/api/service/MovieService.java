dac77de28e7e839bbb672dbeb6b090353654d350
Halil Bagci <halilbagci75@hotmail.com>
2021-01-25 12:04:36 +0100

    Update MovieService to extract and add director to view

--- src/main/java/com/netflix/api/service/MovieService.java
+++ src/main/java/com/netflix/api/service/MovieService.java
@@ -2,6 +2,7 @@ package com.netflix.api.service;
 
 import com.netflix.api.client.MovieClient;
 import com.netflix.api.client.MovieLogoClient;
+import com.netflix.api.dto.Crew;
 import com.netflix.api.dto.MovieDetailsDto;
 import com.netflix.api.dto.MovieLogoDto;
 import com.netflix.api.dto.Result;
@@ -28,7 +29,7 @@ public class MovieService {
     private String fanartApiKey;
 
     private String language = "en-US";
-//    combinatie van
+//    combinatie van videos en credits voor de append to usage call
     private String videosAndCredits = "videos,credits";
 //    private String credits = "credits";
 
@@ -50,15 +51,22 @@ public class MovieService {
         view.setRuntime(movie.getRuntime());
         view.setGenres(movie.getGenres());
         view.setHdmovielogo(logo.getHdmovielogo());
+
         view.setMoviethumb(logo.getMoviethumb());
 //       v will set videos with results with lists of trailer info
 //    view.setVideos(movie.getVideos());
 //       v will set result with list of trailer info
-        view.setResult(movie.getVideos().getResults());
+//        view.setResult(movie.getVideos().getResults());
         List<Result> listForYoutubeKey = movie.getVideos().getResults();
         view.setYoutubeKey(listForYoutubeKey.get(0));
-        
-        view.setCredits(movie.getCredits());
+
+//        view.setCredits(movie.getCredits());
+        view.setDirector(movie.getCredits());
+//        List<Crew> listOfCrewForDirector = movie.getCredits().getCrew();
+//        if(listOfCrewForDirector)
+//        System.out.println(listOfCrewForDirector);
+//        System.out.println("list director =  " + listOfCrewForDirector.stream().filter(t -> t.getJob().equals("Director")).findFirst().get());
+//            view.setDirector(listOfCrewForDirector.stream().filter(t -> t.getJob().equals("Director")).findFirst().get());
         return view;
     }
 }
