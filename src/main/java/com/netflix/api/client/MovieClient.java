2c1cbcab1f0e8414fc38bd8c58cbe57203537002
Halil Bagci <halilbagci75@hotmail.com>
2021-02-05 17:58:08 +0100

    Add genre endpoint to the feignclient

--- src/main/java/com/netflix/api/client/MovieClient.java
+++ src/main/java/com/netflix/api/client/MovieClient.java
@@ -1,6 +1,7 @@
 package com.netflix.api.client;
 
 import com.netflix.api.dto.MovieDetailsDto;
+import com.netflix.api.genresdto.GenreID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,5 +14,27 @@ public interface MovieClient {
 //  public MovieDetailsDto getMovieDetails(@PathVariable String movieId, @RequestParam(value = "api_key") String apiKey);
 //}
   @GetMapping("/movie/{movieId}")
-  public MovieDetailsDto getMovieDetails(@PathVariable String movieId, @RequestParam(value = "api_key") String apiKey,@RequestParam(value = "language") String language, @RequestParam( value = "append_to_response") String videosAndCredits);
+  public MovieDetailsDto getMovieDetails(@PathVariable String movieId,
+                                         @RequestParam(value = "api_key") String apiKey,
+                                         @RequestParam(value = "language") String language,
+                                         @RequestParam( value = "append_to_response") String videosAndCredits);
+
+  @GetMapping("/discover/movie")
+  public GenreID getGenreIDs (@RequestParam(value = "api_key", required=false) String apiKey,
+                                 @RequestParam( value = "with_genres", required=false) String genre
+//          ,
+//                                @RequestParam(value = "language", required=false) String language,
+//                                 @RequestParam( value = "with_decade", required=false) String decade,
+//                                 @RequestParam(value = "with_cast", required=false) String cast,
+//                                 @RequestParam(value = "with_company", required=false)String company,
+//                                 @RequestParam(value = "with_page", required=false)Integer page,
+//                                 @RequestParam(value = "primary_release_date.gte", required=false)String startDate,
+//                                 @RequestParam(value = "primary_release_date.lte", required=false)String endDate
+                                    );
+
+    @GetMapping("/discover/movie")
+    public GenreID getIDForCast (@RequestParam(value = "api_key", required=false) String apiKey,
+                                @RequestParam(value = "with_cast", required=false) String cast
+
+    );
 }
