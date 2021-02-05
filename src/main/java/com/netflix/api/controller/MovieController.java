23778e560177ae40febee9bc0835bac635ae068f
Halil Bagci <halilbagci75@hotmail.com>
2021-02-05 17:57:46 +0100

    Add Genre endpoint to the controller

--- src/main/java/com/netflix/api/controller/MovieController.java
+++ src/main/java/com/netflix/api/controller/MovieController.java
@@ -1,22 +1,34 @@
 package com.netflix.api.controller;
 
+import com.netflix.api.genresdto.GenreID;
+import com.netflix.api.service.GenreService;
 import com.netflix.api.service.MovieService;
 import com.netflix.api.view.MovieView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MovieController {
 
-  @Autowired
-  private MovieService service;
+    @Autowired
+    private MovieService service;
+    @Autowired
+    private GenreService genreService;
 
-  @CrossOrigin
-  @GetMapping("/movies/{movieId}")
-  public MovieView getMovie(@PathVariable String movieId) {
-    return service.getBannerMovie(movieId);
-  }
+    @CrossOrigin
+    @GetMapping("/movies/{movieId}")
+    public MovieView getMovie(@PathVariable String movieId) {
+        return service.getBannerMovie(movieId);
+    }
+
+    @GetMapping("/movies/genres")
+    public GenreID getGenreMovies(@RequestParam(value = "with_genres", required=false) String genre,
+                                  @RequestParam(value = "language", required=false) String language,
+                                 @RequestParam(value = "with_cast", required=false) String cast,
+                                 @RequestParam(value = "with_company", required=false) String company,
+                                 @RequestParam(value = "with_decade", required=false) String decade
+    ) {
+        return genreService.getGenreMovies(genre, language, cast, company, decade);
+//        return genreService.getGenreMovies(genre, language);
+    }
 }
