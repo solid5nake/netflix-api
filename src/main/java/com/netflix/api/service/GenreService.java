507cede9230d3ffc34697775fb552a99c8e24c67
Halil Bagci <halilbagci75@hotmail.com>
2021-02-08 15:04:48 +0100

    Update GenreService to combine 20 movies to 40 total
    only fantasy works at the moment

--- src/main/java/com/netflix/api/service/GenreService.java
+++ src/main/java/com/netflix/api/service/GenreService.java
@@ -1,11 +1,16 @@
 package com.netflix.api.service;
 
 import com.netflix.api.client.MovieClient;
-import com.netflix.api.dto.MovieDetailsDto;
+import com.netflix.api.genresdto.Result;
 import com.netflix.api.genresdto.GenreID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class GenreService {
@@ -21,7 +26,7 @@ public class GenreService {
 
     private String language = "en-US";
 
-    private Integer page = 1;
+    private Integer page = 2;
     enum Element{
     H("Hydrogen"),
     HE("Helium"),
@@ -59,11 +64,32 @@ public class GenreService {
 //    }
 //    return null;
 //}
-    public GenreID getMoviesByGenre(String genre) {
+    public List<Result> getMoviesByGenre(String genre) {
 
-        return genreClient.getMoviesByGenre(tmdbApiKey, page, genre, language);
+//        switch (genre.toLowerCase(Locale.ROOT)) {
+       return switch (genre) {
+//            case  "comedy"-> genreClient.getMoviesByGenre(tmdbApiKey, page, "35", language);
+//            case "action" ->  genreClient.getMoviesByGenre(tmdbApiKey, page, "28", language);
+//            case "thriller" ->  genreClient.getMoviesByGenre(tmdbApiKey, page, "53", language);
+//            case "family" ->  genreClient.getMoviesByGenre(tmdbApiKey, page, "10751", language);
+//            case "fantasy" ->  genreClient.getMoviesByGenre(tmdbApiKey, page, "14", language);
+           case "fantasy" ->  get40MoviesByGenre("14");
+//            case "crime" ->  genreClient.getMoviesByGenre(tmdbApiKey, page, "80", language);
+//            case "adventure" -> genreClient.getMoviesByGenre(tmdbApiKey, page, "12", language);
+           default -> throw new IllegalStateException("Unexpected value: " + genre);
+       };
+//        return genreClient.getMoviesByGenre(tmdbApiKey, page, genre, language);
     }
+private List<Result> get40MoviesByGenre(String genreNumberTMDB){
+    GenreID first20Movies = genreClient.getMoviesByGenre(tmdbApiKey, 1, genreNumberTMDB, language);
+    List<Result> first20Results = first20Movies.getResults();
+    GenreID second20Movies = genreClient.getMoviesByGenre(tmdbApiKey, 2, genreNumberTMDB, language);
+    List<Result> second20Results = second20Movies.getResults();
+    List<Result> total40movies = new ArrayList<>();
+    Stream.of(first20Results, second20Results).forEach(total40movies::addAll);
 
+    return total40movies;
+}
     public GenreID getMoviesByCompany(String company) {
         return genreClient.getMoviesByCompany(tmdbApiKey, page, company, language);
     }
