5db5af41d726bb8b24314c912e660ef0d71cf2e1
Halil Bagci <halilbagci75@hotmail.com>
2021-02-08 11:44:32 +0100

    Add methods to genreService to call based on cast, company and genre

--- src/main/java/com/netflix/api/service/GenreService.java
+++ src/main/java/com/netflix/api/service/GenreService.java
@@ -9,29 +9,70 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GenreService {
+
     @Autowired
     private MovieClient genreClient;
+
     @Value("${tmdb.api_key}")
     private String tmdbApiKey;
 
     @Value("${fanart.api_key}")
     private String fanartApiKey;
 
+    private String language = "en-US";
+
+    private Integer page = 1;
+    enum Element{
+    H("Hydrogen"),
+    HE("Helium"),
+    NE("Neon");
+
+        public final String ID;
+
+        private Element(String ID) {
+            this.ID = ID;
+        }
+
+}
+
 //    public String getGenreMovies(String genre, String cast, String company, String decade) {
 //        System.out.print(genre + cast + company + decade);
 //        return genre;
 //    }
-    public GenreID getGenreMovies(String genre, String language, String  cast, String company, String decade) {
-//        MovieDetailsDto movie = client.getMovieDetails(movieId, tmdbApiKey, language, videosAndCredits);
-        GenreID genreId = genreClient.getGenreIDs(tmdbApiKey, genre);
-        System.out.print(genreId);
-        return genreId;
-    }
-    public GenreID getCastMovies(String cast) {
-//        MovieDetailsDto movie = client.getMovieDetails(movieId, tmdbApiKey, language, videosAndCredits);
-        GenreID genreId = genreClient.getIDForCast(tmdbApiKey, cast);
-        System.out.print(genreId);
-        return genreId;
+//    public GenreID getGenreMovies(String genre, String language, String  cast, String company, String decade) {
+////        MovieDetailsDto movie = client.getMovieDetails(movieId, tmdbApiKey, language, videosAndCredits);
+//        GenreID genreId = genreClient.getGenreIDs(tmdbApiKey, genre,);
+//        System.out.print(genreId);
+//        return genreId;
+//    }
+//    public GenreID getGenreMovies(String cast) {
+////        MovieDetailsDto movie = client.getMovieDetails(movieId, tmdbApiKey, language, videosAndCredits);
+//        GenreID genreId = genreClient.getIDForCast(tmdbApiKey, cast);
+//        System.out.print(genreId);
+//        return genreId;
+//    }
+//public static Element valueOfLabel(String ID) {
+//    for (Element e : values()) {
+//        if (e.ID.equals(ID)) {
+//            return e;
+//        }
+//    }
+//    return null;
+//}
+    public GenreID getMoviesByGenre(String genre) {
+
+        return genreClient.getMoviesByGenre(tmdbApiKey, page, genre, language);
     }
 
+    public GenreID getMoviesByCompany(String company) {
+        return genreClient.getMoviesByCompany(tmdbApiKey, page, company, language);
+    }
+
+    public GenreID getMoviesByCast(String cast) {
+        return genreClient.getMoviesByCast(tmdbApiKey, page, cast, language);
+    }
+
+//    public GenreID getMoviesByDecade(String decade) {
+//        return genreClient.getMoviesByCast(tmdbApiKey, cast, language);
+//    }
 }
