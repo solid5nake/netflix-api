4b7fe37573c92f5b24b112c1729ae5155e700097
Halil Bagci <halilbagci75@hotmail.com>
2021-02-08 11:45:06 +0100

    Add methods to controller to call based on cast, company and genre

--- src/main/java/com/netflix/api/controller/MovieController.java
+++ src/main/java/com/netflix/api/controller/MovieController.java
@@ -22,13 +22,30 @@ public class MovieController {
     }
 
     @GetMapping("/movies/genres")
-    public GenreID getGenreMovies(@RequestParam(value = "with_genres", required=false) String genre,
-                                  @RequestParam(value = "language", required=false) String language,
-                                 @RequestParam(value = "with_cast", required=false) String cast,
-                                 @RequestParam(value = "with_company", required=false) String company,
-                                 @RequestParam(value = "with_decade", required=false) String decade
+    public GenreID getGenreMovies(@RequestParam(value = "with_genres", required = false) String genre,
+                                  @RequestParam(value = "language", required = false) String language,
+                                  @RequestParam(value = "with_cast", required = false) String cast,
+                                  @RequestParam(value = "with_company", required = false) String company,
+                                  @RequestParam(value = "with_decade", required = false) String decade
     ) {
-        return genreService.getGenreMovies(genre, language, cast, company, decade);
+
+//        return genreService.getGenreMovies(genre, language, cast, company, decade);
 //        return genreService.getGenreMovies(genre, language);
+        if (genre != null)
+            return genreService.getMoviesByGenre(genre);
+        else if (cast != null)
+           return  genreService.getMoviesByCast(cast);
+        else if (company != null)
+         return  genreService.getMoviesByCompany(company);
+//       else if (decade != null){
+//          return  genreService.getMoviesByDecade(decade);
+//        }
+       else
+//        System.out.println("genre= " + genre);
+//        System.out.println("language= " + language);
+//        System.out.println("cast= " + cast);
+        System.out.println("company= " + company);
+//        System.out.println("decade= " + decade);
+        return new GenreID();
     }
 }
