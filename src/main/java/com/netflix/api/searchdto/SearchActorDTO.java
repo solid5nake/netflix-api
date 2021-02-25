fe8952b28f6a034a1922c2db341503c3afe02a25
Halil Bagci <halilbagci75@hotmail.com>
2021-02-25 15:35:14 +0100

    Add  DTO's to search for actors based on input

---
+++ src/main/java/com/netflix/api/searchdto/SearchActorDTO.java
@@ -0,0 +1,29 @@
+
+package com.netflix.api.searchdto;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.netflix.api.dto.Result;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "results"
+})
+public class SearchActorDTO {
+
+    @JsonProperty("results")
+    private List<Result> results = null;
+
+    @JsonProperty("results")
+    public List<Result> getResults() {
+        return results;
+    }
+
+    @JsonProperty("results")
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+}
